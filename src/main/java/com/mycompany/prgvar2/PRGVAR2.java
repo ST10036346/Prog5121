7398d296aed1af030d09843e274f69f16145892b
ST10036346 <ST10036346@vcconnect.edu.za>
2023-04-06 06:32:51 +0000

    first commit

---
+++ src/main/java/com/mycompany/prgvar2/PRGVAR2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prgvar2;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class PRGVAR2 
+{
+
+    public static void main(String[] args) 
+    {
+        String Name = "Kevin Jones";
+        Name = "Mr " + Name;
+        int Age = 17;
+        double Weight = 150.34;
+        
+        System.out.println("Personal Details");
+        System.out.println("*********************");
+        System.out.println("Name: " + Name);
+        System.out.println("Age: " + Age);
+        System.out.println("Weight: " + Weight);
+    }
+}
